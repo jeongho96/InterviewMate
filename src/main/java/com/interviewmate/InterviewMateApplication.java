9419e04a75d0a2357091f901181764080282d9f3
jeongho96 <leejh9128@gmail.com>
2024-09-22 11:38:17 +0900

    project init

---
+++ src/main/java/com/interviewmate/InterviewMateApplication.java
@@ -0,0 +1,13 @@
+package com.interviewmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewMateApplication.class, args);
+    }
+
+}
